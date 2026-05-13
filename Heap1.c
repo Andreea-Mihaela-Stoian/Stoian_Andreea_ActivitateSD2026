bff6fce0cc33602db04a67ded5e0cda395edd6dd
Andreea-Mihaela-Stoian <andreea.mihaela3303@gmail.com>
2026-05-13 20:38:12 +0300

    continuare exercitiu Heap

--- Heap1.c
+++ Heap1.c
@@ -2,3 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+struct StructuraTren {
+	int id;
+	int nrVagoane;
+	float pretBilet;
+	char* locomotiva;
+	char* numeTren;
+	unsigned char serie;
+};
+typedef struct StructuraTren Tren;
