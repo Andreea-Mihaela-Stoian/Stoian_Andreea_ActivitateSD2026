5bff65b502236367e229596201c0da3fb6d73a13
Andreea-Mihaela-Stoian <andreea.mihaela3303@gmail.com>
2026-05-14 22:48:37 +0300

    Am adaugat inserarea si afisarea tabelei hash

--- HashTable_Angajati/HashTable_Angajati.c
+++ HashTable_Angajati/HashTable_Angajati.c
@@ -81,3 +81,108 @@ void afisareAngajat(Angajat angajat) {
 	printf("Departament: %s\n", angajat.departament);
 	printf("Cod: %c\n\n", angajat.cod);
 }
+// Afiseaza lista simplu inlantuita
+void afisareListaAngajati(Nod* cap) {
+	while (cap) {
+		afisareAngajat(cap->info);
+		cap = cap->next;
+	}
+}
+
+// Adauga angajat la finalul listei
+void adaugaAngajatInLista(Nod** cap, Angajat angajatNou) {
+
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+
+	nou->info = angajatNou;
+	nou->next = NULL;
+
+	if (*cap == NULL) {
+		*cap = nou;
+	}
+	else {
+		Nod* aux = *cap;
+
+		while (aux->next) {
+			aux = aux->next;
+		}
+
+		aux->next = nou;
+	}
+}
+
+// Initializare HashTable
+HashTable initializareHashTable(int dimensiune) {
+
+	HashTable ht;
+
+	ht.dim = dimensiune;
+
+	ht.vector = (Nod**)malloc(sizeof(Nod*) * dimensiune);
+
+	for (int i = 0; i < dimensiune; i++) {
+		ht.vector[i] = NULL;
+	}
+
+	return ht;
+}
+
+// Calcul hash
+int calculeazaHash(int id, int dimensiune) {
+
+	// folosim modulo pentru a obtine pozitia
+	return id % dimensiune;
+}
+
+// Inserare angajat in tabela
+void inserareAngajatInTabela(HashTable ht, Angajat angajat) {
+
+	int pozitie =
+		calculeazaHash(angajat.id, ht.dim);
+
+	adaugaAngajatInLista(
+		&(ht.vector[pozitie]),
+		angajat);
+}
+
+// Citire angajati din fisier
+HashTable citireAngajatiDinFisier(
+	const char* numeFisier) {
+
+	HashTable ht =
+		initializareHashTable(5);
+
+	FILE* f = fopen(numeFisier, "r");
+
+	if (f) {
+
+		while (!feof(f)) {
+
+			Angajat a =
+				citireAngajatDinFisier(f);
+
+			if (a.id != -1) {
+
+				inserareAngajatInTabela(
+					ht,
+					a);
+			}
+		}
+
+		fclose(f);
+	}
+
+	return ht;
+}
+
+// Afisare tabela
+void afisareTabelaDeAngajati(HashTable ht) {
+
+	for (int i = 0; i < ht.dim; i++) {
+
+		printf("\nCluster %d:\n", i);
+
+		afisareListaAngajati(
+			ht.vector[i]);
+	}
+}
