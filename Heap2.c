98b1a0e4b77996a60b8b38c8d2b622e29fd16c87
Andreea-Mihaela-Stoian <andreea.mihaela3303@gmail.com>
2026-05-14 19:07:04 +0300

    HEAP 2

--- Heap2.c
+++ Heap2.c
@@ -142,3 +142,91 @@ Heap citireHeapDeStudentiDinFisier(const char* numeFisier) {
 
 	return heap;
 }
+void afisareHeap(Heap heap) {
+	// afiseaza studentii vizibili din heap
+
+	for (int i = 0; i < heap.nrElemente; i++) {
+		afisareStudent(heap.studenti[i]);
+	}
+}
+
+void afiseazaHeapAscuns(Heap heap) {
+	// afiseaza studentii extrasi
+	// ei nu sunt stersi, doar mutati in zona ascunsa
+
+	for (int i = heap.nrElemente; i < heap.lungime; i++) {
+		afisareStudent(heap.studenti[i]);
+	}
+}
+
+Student extrageStudent(Heap* heap) {
+	// extrage studentul de pe prima pozitie
+	// in MAX-HEAP, acesta are media cea mai mare
+
+	Student student;
+
+	// valoare default daca heap-ul este gol
+	student.id = -1;
+	student.anStudiu = 0;
+	student.medie = 0;
+	student.nume = NULL;
+	student.specializare = NULL;
+	student.grupa = '-';
+
+	if (heap->nrElemente > 0) {
+		student = heap->studenti[0];
+
+		// mutam ultimul element vizibil pe prima pozitie
+		heap->studenti[0] = heap->studenti[heap->nrElemente - 1];
+
+		// punem studentul extras la final
+		heap->studenti[heap->nrElemente - 1] = student;
+
+		// scadem numarul de elemente vizibile
+		heap->nrElemente--;
+
+		// refacem heap-ul
+		for (int i = (heap->nrElemente - 2) / 2; i >= 0; i--) {
+			filtreazaHeap(*heap, i);
+		}
+	}
+
+	return student;
+}
+
+void dezalocareHeap(Heap* heap) {
+	// eliberam memoria pentru campurile alocate dinamic
+
+	for (int i = 0; i < heap->lungime; i++) {
+		free(heap->studenti[i].nume);
+		free(heap->studenti[i].specializare);
+	}
+
+	// eliberam vectorul
+	free(heap->studenti);
+
+	heap->studenti = NULL;
+	heap->lungime = 0;
+	heap->nrElemente = 0;
+}
+
+int main() {
+	Heap heap = citireHeapDeStudentiDinFisier("studenti.txt");
+
+	printf("Heap initial:\n");
+	afisareHeap(heap);
+
+	printf("Extrageri:\n");
+	afisareStudent(extrageStudent(&heap));
+	afisareStudent(extrageStudent(&heap));
+
+	printf("Heap ramas vizibil:\n");
+	afisareHeap(heap);
+
+	printf("Elemente ascunse:\n");
+	afiseazaHeapAscuns(heap);
+
+	dezalocareHeap(&heap);
+
+	return 0;
+}
