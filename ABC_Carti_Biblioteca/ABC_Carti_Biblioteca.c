86ec83c823ba7e47675ee9d106ea5c3d2c09b919
Andreea-Mihaela-Stoian <andreea.mihaela3303@gmail.com>
2026-05-14 20:49:20 +0300

    Problema ABC rezolvata

--- ABC_Carti_Biblioteca/ABC_Carti_Biblioteca.c
+++ ABC_Carti_Biblioteca/ABC_Carti_Biblioteca.c
@@ -142,3 +142,128 @@ void afisarePreordine(Nod* rad) {
 		afisarePreordine(rad->dreapta);
 	}
 }
+void dezalocareArboreDeCarti(Nod** rad) {
+	// dezaloca toate nodurile si campurile alocate dinamic
+
+	if (*rad) {
+		dezalocareArboreDeCarti(&(*rad)->stanga);
+		dezalocareArboreDeCarti(&(*rad)->dreapta);
+
+		free((*rad)->info.titlu);
+		free((*rad)->info.autor);
+		free(*rad);
+
+		*rad = NULL;
+	}
+}
+
+Carte getCarteByID(Nod* rad, int id) {
+	// cauta o carte dupa id
+
+	Carte c;
+	c.id = -1;
+	c.anPublicare = 0;
+	c.pret = 0;
+	c.titlu = NULL;
+	c.autor = NULL;
+	c.codRaft = '-';
+
+	if (rad) {
+		if (rad->info.id == id) {
+			c = rad->info;
+
+			c.titlu = (char*)malloc(strlen(rad->info.titlu) + 1);
+			strcpy_s(c.titlu, strlen(rad->info.titlu) + 1, rad->info.titlu);
+
+			c.autor = (char*)malloc(strlen(rad->info.autor) + 1);
+			strcpy_s(c.autor, strlen(rad->info.autor) + 1, rad->info.autor);
+		}
+		else if (id < rad->info.id) {
+			c = getCarteByID(rad->stanga, id);
+		}
+		else {
+			c = getCarteByID(rad->dreapta, id);
+		}
+	}
+
+	return c;
+}
+
+int determinaNumarNoduri(Nod* rad) {
+	if (rad) {
+		return determinaNumarNoduri(rad->stanga)
+			+ determinaNumarNoduri(rad->dreapta)
+			+ 1;
+	}
+
+	return 0;
+}
+
+int maxim(int a, int b) {
+	return a > b ? a : b;
+}
+
+int calculeazaInaltimeArbore(Nod* rad) {
+	if (rad) {
+		return maxim(calculeazaInaltimeArbore(rad->stanga),
+			calculeazaInaltimeArbore(rad->dreapta)) + 1;
+	}
+
+	return 0;
+}
+
+float calculeazaPretTotal(Nod* rad) {
+	// calculeaza valoarea totala a cartilor
+
+	if (rad) {
+		return rad->info.pret
+			+ calculeazaPretTotal(rad->stanga)
+			+ calculeazaPretTotal(rad->dreapta);
+	}
+
+	return 0;
+}
+
+int numaraCartiDupaAutor(Nod* rad, const char* autor) {
+	// numara cartile scrise de un anumit autor
+
+	if (rad) {
+		int nr = numaraCartiDupaAutor(rad->stanga, autor)
+			+ numaraCartiDupaAutor(rad->dreapta, autor);
+
+		if (strcmp(rad->info.autor, autor) == 0) {
+			nr++;
+		}
+
+		return nr;
+	}
+
+	return 0;
+}
+
+int main() {
+	Nod* rad = citireArboreDeCartiDinFisier("carti.txt");
+
+	printf("Afisare preordine:\n");
+	afisarePreordine(rad);
+
+	printf("Afisare inordine:\n");
+	afisareCartiInordine(rad);
+
+	printf("Carte cautata:\n");
+	Carte carteCautata = getCarteByID(rad, 8);
+	afisareCarte(carteCautata);
+
+	free(carteCautata.titlu);
+	free(carteCautata.autor);
+
+	printf("Numar noduri: %d\n", determinaNumarNoduri(rad));
+	printf("Inaltime arbore: %d\n", calculeazaInaltimeArbore(rad));
+	printf("Pret total carti: %.2f\n", calculeazaPretTotal(rad));
+	printf("Numar carti de Liviu Rebreanu: %d\n",
+		numaraCartiDupaAutor(rad, "Liviu Rebreanu"));
+
+	dezalocareArboreDeCarti(&rad);
+
+	return 0;
+}
