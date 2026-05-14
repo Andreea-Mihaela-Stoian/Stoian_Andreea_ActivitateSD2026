5e6799fd049ad0be62b03a229b6835d944e3d4dd
Andreea-Mihaela-Stoian <andreea.mihaela3303@gmail.com>
2026-05-14 18:57:34 +0300

    Am adaugat initializarea si construirea heap-ului de studenti

--- Heap2.c
+++ Heap2.c
@@ -69,3 +69,76 @@ void afisareStudent(Student student) {
 	printf("Specializare: %s\n", student.specializare);
 	printf("Grupa: %c\n\n", student.grupa);
 }
+Heap initializareHeap(int lungime) {
+	// initializam un heap gol, dar cu spatiu alocat
+
+	Heap heap;
+
+	// capacitatea maxima a heap-ului
+	heap.lungime = lungime;
+
+	// vectorul in care stocam studentii
+	heap.studenti = (Student*)malloc(sizeof(Student) * lungime);
+
+	// initial nu avem studenti in heap
+	heap.nrElemente = 0;
+
+	return heap;
+}
+
+void filtreazaHeap(Heap heap, int pozitieNod) {
+	// functia reface proprietatea de MAX-HEAP
+	// criteriul folosit este media studentului
+
+	int pozStanga = 2 * pozitieNod + 1;
+	int pozDreapta = 2 * pozitieNod + 2;
+
+	// presupunem ca nodul curent are media cea mai mare
+	int pozMax = pozitieNod;
+
+	// verificam fiul stang
+	if (pozStanga < heap.nrElemente &&
+		heap.studenti[pozMax].medie < heap.studenti[pozStanga].medie) {
+		pozMax = pozStanga;
+	}
+
+	// verificam fiul drept
+	if (pozDreapta < heap.nrElemente &&
+		heap.studenti[pozMax].medie < heap.studenti[pozDreapta].medie) {
+		pozMax = pozDreapta;
+	}
+
+	// daca un fiu are media mai mare, facem interschimbarea
+	if (pozMax != pozitieNod) {
+		Student aux = heap.studenti[pozMax];
+		heap.studenti[pozMax] = heap.studenti[pozitieNod];
+		heap.studenti[pozitieNod] = aux;
+
+		// continuam filtrarea pe pozitia unde a ajuns nodul mutat
+		filtreazaHeap(heap, pozMax);
+	}
+}
+
+Heap citireHeapDeStudentiDinFisier(const char* numeFisier) {
+	// citim studentii din fisier
+	// apoi transformam vectorul intr-un MAX-HEAP dupa medie
+
+	Heap heap = initializareHeap(10);
+
+	FILE* f = fopen(numeFisier, "r");
+
+	if (f) {
+		while (!feof(f)) {
+			heap.studenti[heap.nrElemente++] = citireStudentDinFisier(f);
+		}
+
+		fclose(f);
+	}
+
+	// filtram de la ultimul parinte pana la radacina
+	for (int i = (heap.nrElemente - 2) / 2; i >= 0; i--) {
+		filtreazaHeap(heap, i);
+	}
+
+	return heap;
+}
